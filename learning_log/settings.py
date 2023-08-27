8d1aeea5a729a7b91d7666a86078c7b1db5e35c4
BeZd4r <103284584+BeZd4r@users.noreply.github.com>
2023-08-27 15:00:12 +0300

    allow_host

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-*2!+tw_550tkzz-gkrp)^chbem9gb_ab*s4!_4(r@ne1)-sai-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['bezdarino.pythonanywhere.com']
 
 
 # Application definition
